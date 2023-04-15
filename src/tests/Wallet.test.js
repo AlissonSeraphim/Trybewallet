a4707dc21e3f0026c3a8aa92a6f9fbff0f15c654
AlissonSeraphim <projetoseraphim@gmail.com>
2023-04-15 00:05:24 -0300

    fixed: Requisito 5

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -57,85 +57,25 @@ describe('Testa a page Wallet', () => {
   });
 
   it('Verifica se ao clicar no botão de despesa a despesa é salva no estado global na chave expenses', async () => {
-    // const initialState = {
-    //   expenses: [
-    //     {
-    //       id: 0,
-    //       value: '150',
-    //       currency: 'USD',
-    //       method: 'Cartão de crédito',
-    //       tag: 'Trabalho',
-    //       description: 'SSD',
-    //       exchangeRates: {
-    //         USD: mockData,
-    //       },
-    //     },
-    //   ],
-    // };
-
-    const initialState = {
-      currencies: [
-        'USD',
-        'CAD',
-        'GBP',
-        'ARS',
-        'BTC',
-        'LTC',
-        'EUR',
-        'JPY',
-        'CHF',
-        'AUD',
-        'CNY',
-        'ILS',
-        'ETH',
-        'XRP',
-        'DOGE',
-      ],
-    };
-
-    const { store } = renderWithRouterAndRedux(<App />, { initialEntries: ['/carteira'], initialState });
+    const { store } = renderWithRouterAndRedux(<App />, { initialEntries: ['/carteira'] });
 
     const expenseInput = screen.getByTestId('value-input');
     const descriptionInput = screen.getByTestId('description-input');
     const currencyInput = screen.getByTestId('currency-input');
-    // const methodInput = screen.getByTestId('method-input');
-    // const tagInput = screen.getByTestId('tag-input');
     const expenseButton = screen.getByRole('button', { name: /adicionar despesa/i });
 
     const expenseValue = 150;
     const descriptionValue = 'SSD';
-    // const currencyValue = 'USD';
-    // const methodValue = 'Cartão de crédito';
-    // const tagValue = 'Trabalho';
-    // const textTest = screen.getByText(/USD/i);
-    const currencies = [
-      'USD',
-      'CAD',
-      'GBP',
-      'ARS',
-      'BTC',
-      'LTC',
-      'EUR',
-      'JPY',
-      'CHF',
-      'AUD',
-      'CNY',
-      'ILS',
-      'ETH',
-      'XRP',
-      'DOGE',
-    ];
 
     userEvent.type(expenseInput, expenseValue);
     userEvent.type(descriptionInput, descriptionValue);
-    // userEvent.selectOptions(currencyInput, random);
-    // userEvent.type(methodInput, methodValue);
-    // userEvent.type(tagInput, tagValue);
 
     expect(currencyInput).toBeInTheDocument();
 
     act(() => userEvent.click(expenseButton));
 
-    await waitFor(() => expect(store.getState().wallet).toHaveProperty('currencies', currencies));
+    await waitFor(() => {
+      expect(store.getState().wallet.expenses[0].description).toBe(descriptionValue);
+    });
   });
 });
